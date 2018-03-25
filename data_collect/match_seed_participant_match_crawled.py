bda152cd62179936e81f1cfa5bdd606879ab7880
czxttkl <czxttkl@gmail.com>
2018-03-25 10:02:53 -0400

    fix match seed link to match

--- data_collect/match_seed_participant_match_crawled.py
+++ data_collect/match_seed_participant_match_crawled.py
@@ -1,16 +1,21 @@
 from data_collect.mypymongo import MyPyMongo
+import pymongo
+
 
 if __name__ == "__main__":
     mypymongo = MyPyMongo()
 
     for match in mypymongo.db.match_seed.find({'$or': [
-                                                    {'participants_matches_crawled': None},
-                                                    {'participants_matches_crawled': False}
-                                                 ]}):
+                                                    {'all_participants_matches_crawled': None},
+                                                    {'all_participants_matches_crawled': False}
+                                                 ]}).sort("_id", pymongo.ASCENDING):  # correspond to match.py
+        all_participants_matches_crawled = True
         for participant in match['participants']:
             account_id = participant['accountId']
             player = mypymongo.find_player_in_player_seed(account_id)
-            participant['participant_in_player_seed'] = True
+            if not player['player_in_match']:
+                all_participants_matches_crawled = False
+                break
 
-        match['participants_matches_crawled'] = True
+        match['all_participants_matches_crawled'] = all_participants_matches_crawled
         mypymongo.db.match_seed.find_one_and_replace({'id': match['id']}, match)
