c12a19ea8009323642185a3adf8ea233a2e74949
czxttkl <czxttkl@gmail.com>
2018-03-31 11:53:10 -0400

    update tf fm

--- model/tffm/models.py
+++ model/tffm/models.py
@@ -1,15 +1,8 @@
 """Implementation of an arbitrary order Factorization Machines."""
 
-import os
 import numpy as np
-import tensorflow as tf
-import shutil
-from tqdm import tqdm
-import utils
-
-from .core import TFFMCore
 from .base import TFFMBaseModel
-from .utils import loss_logistic, loss_mse
+from .utils import loss_logistic, loss_mse,  sigmoid
 
 
 
@@ -21,58 +14,70 @@ class TFFMClassifier(TFFMBaseModel):
 
     Only binary classification with 0/1 labels supported.
 
-    See TFFMBaseModel docs for details about parameters.
+    See TFFMBaseModel and TFFMCore docs for details about parameters.
     """
 
-    def __init__(self, rank=2, order=2, input_type='dense', n_epochs=100,
-                optimizer=tf.train.AdamOptimizer(learning_rate=0.1), reg=0,
-                batch_size=-1, init_std=0.01, log_dir=None, verbose=0,
-                session_config=None, seed=None):
-        init_params = {
-            'rank': rank,
-            'order': order,
-            'input_type': input_type,
-            'n_epochs': n_epochs,
-            'batch_size': batch_size,
-            'reg': reg,
-            'init_std': init_std,
-            'optimizer': optimizer,
-            'log_dir': log_dir,
-            'loss_function': loss_logistic,
-            'session_config': session_config,
-            'verbose': verbose,
-            'seed': seed
-        }
+    def __init__(self, **init_params):
+
+        assert 'loss_function' not in init_params, """Parameter 'loss_function' is
+        not supported for TFFMClassifier. For custom loss function, extend the
+        base class TFFMBaseModel."""
+
+        init_params['loss_function'] = loss_logistic
         self.init_basemodel(**init_params)
 
-    def preprocess_target(self, y_):
-        # suppose input {0, 1}, but use instead {-1, 1} labels
-        assert(set(y_) == set([0, 1]))
-        return y_ * 2 - 1
+    def _preprocess_sample_weights(self, sample_weight, pos_class_weight, used_y):
+        assert sample_weight is None or pos_class_weight is None, "sample_weight and pos_class_weight are mutually exclusive parameters"
+        used_w = np.ones_like(used_y)
+        if sample_weight is None and pos_class_weight is None:
+            return used_w
+        if type(pos_class_weight) == float:
+            used_w[used_y > 0] = pos_class_weight
+        elif sample_weight == "balanced":
+            pos_rate = np.mean(used_y > 0)
+            neg_rate = 1 - pos_rate
+            used_w[used_y > 0] = neg_rate / pos_rate
+            used_w[used_y < 0] = 1.0
+            return used_w
+        elif type(sample_weight) == np.ndarray and len(sample_weight.shape)==1:
+            used_w = sample_weight
+        else:
+            raise ValueError("Unexpected type for sample_weight or pos_class_weight parameters.")
 
-    def predict(self, X):
+        return used_w
+
+
+    def fit(self, X, y, sample_weight=None, pos_class_weight=None, n_epochs=None, show_progress=False):
+        # preprocess Y: suppose input {0, 1}, but internally will use {-1, 1} labels instead
+        if not (set(y) == set([0, 1])):
+            raise ValueError("Input labels must be in set {0,1}.")
+        used_y = y * 2 - 1
+        if sample_weight is not None:
+            self.sample_weight = sample_weight
+        if pos_class_weight is not None:
+            self.pos_class_weight = pos_class_weight
+        used_w = self._preprocess_sample_weights(self.sample_weight, self.pos_class_weight, used_y)
+        self._fit(X_=X, y_=used_y, w_=used_w, n_epochs=n_epochs, show_progress=show_progress)
+
+    def predict(self, X, pred_batch_size=None):
         """Predict using the FM model
 
         Parameters
         ----------
         X : {numpy.array, scipy.sparse.csr_matrix}, shape = (n_samples, n_features)
             Samples.
+        pred_batch_size : int batch size for prediction (default None)
 
         Returns
         -------
         predictions : array, shape = (n_samples,)
             Returns predicted values.
         """
-        raw_output = self.decision_function(X)
+        raw_output = self.decision_function(X, pred_batch_size)
         predictions = (raw_output > 0).astype(int)
         return predictions
 
-    def score(self, X, y):
-        """ Accuracy """
-        predictions = self.predict(X)
-        return np.mean(y == predictions)
-
-    def predict_proba(self, X):
+    def predict_proba(self, X, pred_batch_size=None):
         """Probability estimates.
 
         The returned estimates for all 2 classes are ordered by the
@@ -81,14 +86,18 @@ class TFFMClassifier(TFFMBaseModel):
         Parameters
         ----------
         X : array-like, shape = [n_samples, n_features]
+        pred_batch_size : int batch size for prediction (default None)
 
         Returns
         -------
-        probs : array-like, shape = [n_samples]
+        probs : array-like, shape = [n_samples, 2]
+            Returns the probability of the sample for each class in the model.
         """
-        outputs = self.decision_function(X)
-        probs_positive = utils.sigmoid(outputs)
-        return probs_positive
+        outputs = self.decision_function(X, pred_batch_size)
+        probs_positive = sigmoid(outputs)
+        probs_negative = 1 - probs_positive
+        probs = np.vstack((probs_negative.T, probs_positive.T))
+        return probs.T
 
 
 class TFFMRegressor(TFFMBaseModel):
@@ -97,45 +106,38 @@ class TFFMRegressor(TFFMBaseModel):
     This class implements L2-regularized arbitrary order FM model with MSE
     loss and gradient-based optimization.
 
-    See TFFMBaseModel docs for details about parameters.
+    Custom loss functions are not supported, mean squared error is always
+    used. Any loss function provided in parameters will be overwritten.
+
+    See TFFMBaseModel and TFFMCore docs for details about parameters.
     """
 
-    def __init__(self, rank=2, order=2, input_type='dense', n_epochs=100,
-                optimizer=tf.train.AdamOptimizer(learning_rate=0.1), reg=0,
-                batch_size=-1, init_std=0.01, log_dir=None, verbose=0,
-                session_config=None, seed=None):
-        init_params = {
-            'rank': rank,
-            'order': order,
-            'input_type': input_type,
-            'n_epochs': n_epochs,
-            'batch_size': batch_size,
-            'reg': reg,
-            'init_std': init_std,
-            'optimizer': optimizer,
-            'log_dir': log_dir,
-            'loss_function': loss_mse,
-            'session_config': session_config,
-            'verbose': verbose,
-            'seed': seed
-        }
+    def __init__(self, **init_params):
+
+        assert 'loss_function' not in init_params, """Parameter 'loss_function' is
+        not supported for TFFMRegressor. For custom loss function, extend the
+        base class TFFMBaseModel."""
+        
+        init_params['loss_function'] = loss_mse
         self.init_basemodel(**init_params)
 
-    def preprocess_target(self, y_):
-        return y_
+    def fit(self, X, y, sample_weight=None, n_epochs=None, show_progress=False):
+        sample_weight = np.ones_like(y) if sample_weight is None else sample_weight
+        self._fit(X_=X, y_=y, w_=sample_weight, n_epochs=n_epochs, show_progress=show_progress)
 
-    def predict(self, X):
+    def predict(self, X, pred_batch_size=None):
         """Predict using the FM model
 
         Parameters
         ----------
         X : {numpy.array, scipy.sparse.csr_matrix}, shape = (n_samples, n_features)
             Samples.
+        pred_batch_size : int batch size for prediction (default None)
 
         Returns
         -------
         predictions : array, shape = (n_samples,)
             Returns predicted values.
         """
-        predictions = self.decision_function(X)
+        predictions = self.decision_function(X, pred_batch_size)
         return predictions
--- model/tffm/utils.py
+++ model/tffm/utils.py
@@ -1,12 +1,12 @@
 """Supporting functions for arbitrary order Factorization Machines."""
 
-
-import itertools
-from itertools import combinations_with_replacement, takewhile, count
 import math
-from collections import defaultdict
 import numpy as np
 import tensorflow as tf
+import itertools
+from itertools import combinations_with_replacement, takewhile, count
+from collections import defaultdict
+
 
 def get_shorter_decompositions(basic_decomposition):
     """Returns all arrays simpler than basic_decomposition.
@@ -38,7 +38,6 @@ def get_shorter_decompositions(basic_decomposition):
     variations = defaultdict(lambda: [])
     for curr_len in range(1, len(basic_decomposition)):
         for sum_rule in combinations_with_replacement(range(curr_len), order):
-            i = 0
             sum_rule = np.array(sum_rule)
             curr_pows = np.array([np.sum(sum_rule == i) for i in range(curr_len)])
             curr_pows = curr_pows[curr_pows != 0]
@@ -126,20 +125,86 @@ def powers_and_coefs(order):
     return powers_and_coefs_list
 
 
+def matmul_wrapper(A, B, optype):
+    """Wrapper for handling sparse and dense versions of `tf.matmul` operation.
+
+    Parameters
+    ----------
+    A : tf.Tensor
+    B : tf.Tensor
+    optype : str, {'dense', 'sparse'}
+
+    Returns
+    -------
+    tf.Tensor
+    """
+    with tf.name_scope('matmul_wrapper') as scope:
+        if optype == 'dense':
+            return tf.matmul(A, B)
+        elif optype == 'sparse':
+            return tf.sparse_tensor_dense_matmul(A, B)
+        else:
+            raise NameError('Unknown input type in matmul_wrapper')
+
+
+def pow_wrapper(X, p, optype):
+    """Wrapper for handling sparse and dense versions of `tf.pow` operation.
+
+    Parameters
+    ----------
+    X : tf.Tensor
+    p : int
+    optype : str, {'dense', 'sparse'}
+
+    Returns
+    -------
+    tf.Tensor
+    """
+    with tf.name_scope('pow_wrapper') as scope:
+        if optype == 'dense':
+            return tf.pow(X, p)
+        elif optype == 'sparse':
+            return tf.SparseTensor(X.indices, tf.pow(X.values, p), X.dense_shape)
+        else:
+            raise NameError('Unknown input type in pow_wrapper')
+
+
+def count_nonzero_wrapper(X, optype):
+    """Wrapper for handling sparse and dense versions of `tf.count_nonzero`.
+
+    Parameters
+    ----------
+    X : tf.Tensor (N, K)
+    optype : str, {'dense', 'sparse'}
+
+    Returns
+    -------
+    tf.Tensor (1,K)
+    """
+    with tf.name_scope('count_nonzero_wrapper') as scope:
+        if optype == 'dense':
+            return tf.count_nonzero(X, axis=0, keep_dims=True)
+        elif optype == 'sparse':
+            indicator_X = tf.SparseTensor(X.indices, tf.ones_like(X.values), X.dense_shape)
+            return tf.sparse_reduce_sum(indicator_X, axis=0, keep_dims=True)
+        else:
+            raise NameError('Unknown input type in count_nonzero_wrapper')
+
+
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
 # Predefined loss functions
-# Should take 2 tf.Ops: outputs and targets and should return tf.Op of loss
-# Be carefull about dimentionality -- maybe tf.transpose(outputs) is needed
+# Should take 2 tf.Ops: outputs, targets and should return tf.Op of element-wise losses
+# Be careful about dimensionality -- maybe tf.transpose(outputs) is needed
 
 def loss_logistic(outputs, y):
     margins = -y * tf.transpose(outputs)
     raw_loss = tf.log(tf.add(1.0, tf.exp(margins)))
     return tf.minimum(raw_loss, 100, name='truncated_log_loss')
 
-
 def loss_mse(outputs, y):
     return tf.pow(y -  tf.transpose(outputs), 2, name='mse_loss')
+    
 
