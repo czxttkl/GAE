ef181d85a8eea73a7244e1f811d80b093f3abc48
czxttkl <czxttkl@gmail.com>
2018-04-05 22:41:07 -0400

    add parser density

--- model/baseline_fm_tf.py
+++ model/baseline_fm_tf.py
@@ -22,6 +22,8 @@ if __name__ == "__main__":
     kwargs = parse_parameters()
 
     dataset = 'lol' if not kwargs else kwargs.dataset
+    density = 'sparse' if not kwargs else kwargs.density
+
     feature_config = 'champion_summoner_two_teams' if not kwargs else kwargs.fm_featconfig
     order = 2 if not kwargs else kwargs.fm_order
     rank = 200 if not kwargs else kwargs.fm_rank
@@ -29,7 +31,7 @@ if __name__ == "__main__":
     reg = 0 if not kwargs else kwargs.fm_reg
     print('use parameter: dataset {}, feature_config: {}, order: {}, rank: {}, n_epochs: {}, reg: {}'
           .format(dataset, feature_config, order, rank, n_epochs, reg))
-    reader = parse_reader(dataset, feature_config)
+    reader = parse_reader(dataset, feature_config, density)
 
     fm_model = TFFMClassifier(
                         order=order,
--- utils/parser.py
+++ utils/parser.py
@@ -2,6 +2,7 @@ import optparse
 import sys
 from data_mangle.cv_fold_sparse_reader import CVFoldSparseReader
 from data_mangle.cv_fold_lol_sparse_reader import CVFoldLoLSparseReader
+from data_mangle.cv_fold_dense_reader import CVFoldDenseReader
 from utils import constants
 
 
@@ -11,23 +12,42 @@ def parse_parameters():
         return None
 
     parser = optparse.OptionParser(usage="usage: %prog [options]")
+    # FM
     parser.add_option("--fm_order", dest="fm_order", type="int", default=0)
     parser.add_option("--fm_rank", dest="fm_rank", type="int", default=0)
     parser.add_option("--fm_epoch", dest="fm_epoch", type="int", default=0)
     parser.add_option("--fm_reg", dest="fm_reg", type="float", default=0.0)
     parser.add_option("--fm_featconfig", dest="fm_featconfig", type="string", default='')
+    # NN
+    parser.add_option("--nn_hidden", dest="nn_hidden", type="int", default=0)
+    parser.add_option("--nn_featconfig", dest="nn_featconfig", type="string", default='')
+    # general
+    parser.add_option("--density", dest='density', type='string', default='')
     parser.add_option("--dataset", dest='dataset', type='string', default='')
     (kwargs, args) = parser.parse_args()
     return kwargs
 
 
-def parse_reader(dataset, feature_config):
+def parse_reader(dataset, feature_config, density):
     if dataset == 'lol':
-        reader = CVFoldLoLSparseReader(data_path=constants.lol_pickle, folds=10, feature_config=feature_config)
+        if density == 'sparse':
+            reader = CVFoldLoLSparseReader(data_path=constants.lol_pickle, folds=10, feature_config=feature_config)
+        else:
+            raise NotImplementedError
     elif dataset == 'dota':
-        reader = CVFoldSparseReader(data_path=constants.dota_pickle, folds=10, feature_config=feature_config)
+        if density == 'dense':
+            reader = CVFoldDenseReader(data_path=constants.dota_pickle, folds=10, feature_config=feature_config)
+        elif density == 'sparse':
+            reader = CVFoldSparseReader(data_path=constants.dota_pickle, folds=10, feature_config=feature_config)
+        else:
+            raise NotImplementedError
     elif dataset == 'dota2':
-        reader = CVFoldSparseReader(data_path=constants.dota2_pickle, folds=10, feature_config=feature_config)
+        if density == 'dense':
+            reader = CVFoldDenseReader(data_path=constants.dota2_pickle, folds=10, feature_config=feature_config)
+        elif density == 'sparse':
+            reader = CVFoldSparseReader(data_path=constants.dota2_pickle, folds=10, feature_config=feature_config)
+        else:
+            raise NotImplementedError
     else:
         raise NotImplementedError
     return reader
