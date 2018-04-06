e1e57f3f4a12f7082e8c82496cd8a3707ec6003f
czxttkl <czxttkl@gmail.com>
2018-04-06 11:14:17 -0400

    add nn command

---
+++ commands/nn_interactive.bash
@@ -0,0 +1,4 @@
+python3.6 baseline_nn.py --dataset=dota --nn_featconfig=one_way_one_team --density=dense --nn_hidden=80
+python3.6 baseline_nn.py --dataset=dota --nn_featconfig=one_way_one_team --density=dense --nn_hidden=120
+python3.6 baseline_nn.py --dataset=dota2 --nn_featconfig=one_way_one_team --density=dense --nn_hidden=20
+python3.6 baseline_nn.py --dataset=dota2 --nn_featconfig=one_way_one_team --density=dense --nn_hidden=25
