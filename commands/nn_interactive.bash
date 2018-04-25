2f91a47c672b74adcf1f792fcfc458c7459600a7
czxttkl <czxttkl@gmail.com>
2018-04-25 12:20:30 -0400

    lol2 pickle generator finished

--- commands/nn_interactive.bash
+++ commands/nn_interactive.bash
@@ -7,7 +7,15 @@ python3.6 baseline_nn.py --dataset=dota3 --nn_featconfig=one_way_one_team --dens
 python3.6 baseline_nn.py --dataset=dota3 --nn_featconfig=one_way_one_team --density=dense --nn_hidden=20
 python3.6 baseline_nn.py --dataset=dota3 --nn_featconfig=one_way_one_team --density=dense --nn_hidden=30
 
+python3.6 baseline_nn.py --dataset=lol2 --nn_featconfig=one_way_one_team --density=dense --nn_hidden=1
+python3.6 baseline_nn.py --dataset=lol2 --nn_featconfig=one_way_one_team --density=dense --nn_hidden=2
+python3.6 baseline_nn.py --dataset=lol2 --nn_featconfig=one_way_one_team --density=dense --nn_hidden=3
+python3.6 baseline_nn.py --dataset=lol2 --nn_featconfig=one_way_one_team --density=dense --nn_hidden=4
 python3.6 baseline_nn.py --dataset=lol2 --nn_featconfig=one_way_one_team --density=dense --nn_hidden=5
+
+
 python3.6 baseline_nn.py --dataset=lol2 --nn_featconfig=one_way_one_team --density=dense --nn_hidden=10
 python3.6 baseline_nn.py --dataset=lol2 --nn_featconfig=one_way_one_team --density=dense --nn_hidden=20
+python3.6 baseline_nn.py --dataset=lol2 --nn_featconfig=one_way_one_team --density=dense --nn_hidden=25
 python3.6 baseline_nn.py --dataset=lol2 --nn_featconfig=one_way_one_team --density=dense --nn_hidden=30
+python3.6 baseline_nn.py --dataset=lol2 --nn_featconfig=one_way_one_team --density=dense --nn_hidden=40
