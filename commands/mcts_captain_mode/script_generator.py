0ad6d4c77d1f2e7788d6f5d6ab7ea7baacdda53f
czxttkl <czxttkl@gmail.com>
2018-05-07 09:15:09 -0400

    update script generator

--- commands/mcts_captain_mode/script_generator.py
+++ commands/mcts_captain_mode/script_generator.py
@@ -17,13 +17,13 @@ for arg in args:
 #SBATCH --exclusive
 #SBATCH --partition=ser-par-10g-5
 #SBATCH -N 1
-#SBATCH -D /home/chen.zhe/GAE/model
+#SBATCH -D /home/chen.zhe/GAE/recom_sys_captain_mode
 
 source activate myenv
-work=/home/chen.zhe/GAE/recom_sys
+work=/home/chen.zhe/GAE/recom_sys_captain_mode
 cd $work
 
-python3.6 -u mcts.py --env_path=NN_hiddenunit120_dota.pickle --num_matches=500 --p0={} --p1={} > slurm/{}_out
+python3.6 -u experiment.py --env_path=NN_hiddenunit120_dota.pickle --num_matches=500 --p0={} --p1={} > slurm/{}_out
 """.format(job_name, job_name, job_name, p0_str, p1_str, job_name)
     with open(job_name + '.bash', 'w') as f:
         f.write(s)
